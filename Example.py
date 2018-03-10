b176e00a028353c5f237c8a3082f3b2db55c56d0
Roberto Cattaneo <cattaroby93@hotmail.it>
2018-03-10 16:05:43 +0100

    Try doc 2

---
+++ Example.py
@@ -0,0 +1,12 @@
+"""This module is an example of module for Sphinx documentation
+"""
+
+### ===========================================================================
+def pippo(fileName = "pippo.txt"):
+    """Print a specified fileName (super useful...)
+
+Parameters:
+    fileName (string): Contains the file name to be printed
+"""
+    
+    print(fileName)
